99f3a7f6a6c56267a304117750184b359d725c35
CamiaCL <camilaclary@gmail.com>
2022-04-04 18:19:02 -0500

    :heavy_plus_sign: add bufferline

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,22 @@
+require("bufferline").setup({
+  options = {
+    theme = "termguicolors",
+    show_close_icon = true,
+    always_show_bufferline = false,
+    diagnostics = {"nvim_lsp", "coc",},
+    indicator_icon = '| ',
+    modified_icon = '●',
+    close_icon = '',
+    left_trunc_marker = '',
+    right_trunc_marker = '',
+    diagnostics_indicator = function(_, _, diagnostics_dict)
+      local s = " "
+      for e, n in pairs(diagnostics_dict) do
+        local sym = e == "error" and " "
+          or (e == "warning" and " " or "")
+        s = s .. sym .. n
+      end
+      return s
+    end,
+  },
+})
